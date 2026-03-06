bf8f84a0540baebcc23ffb5a45fb6ddb972a966e
tan <miforbc@gmail.com>
2026-03-07 03:42:06 +0700

    Add try on history to userpage, add new progress bar when using try on AI, confirm redirect to userpage for try on result

--- AI/sd15cpu.py
+++ AI/sd15cpu.py
@@ -3,11 +3,25 @@ import torch
 import cv2
 import numpy as np
 from PIL import Image
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
+from flask_cors import CORS
 from transformers import SegformerImageProcessor, SegformerForSemanticSegmentation
 from diffusers import ControlNetModel, StableDiffusionControlNetInpaintPipeline
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="TF_Database"
+)
+
+cursor = db.cursor()
 
 app = Flask(__name__)
+CORS(app)
+
+progress_dict = {}
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 dtype = torch.float16 if device == "cuda" else torch.float32
@@ -15,7 +29,6 @@ dtype = torch.float16 if device == "cuda" else torch.float32
 print(f"Using device: {device}")
 
 #SEGMENTATION
-
 seg_model_name = "sayeed99/segformer_b3_clothes"
 
 seg_processor = SegformerImageProcessor.from_pretrained(seg_model_name)
@@ -26,7 +39,6 @@ seg_model.eval()
 print("Segmentation model loaded")
 
 #CONTROLNET
-
 controlnet = ControlNetModel.from_pretrained(
     "lllyasviel/control_v11p_sd15_canny",
     torch_dtype=dtype
@@ -48,19 +60,17 @@ else:
 pipe.safety_checker = None
 
 #IP ADAPTER
-
 pipe.load_ip_adapter(
     "h94/IP-Adapter",
     subfolder="models",
     weight_name="ip-adapter-plus_sd15.bin"
 )
 
-pipe.set_ip_adapter_scale(1.0)
+pipe.set_ip_adapter_scale(1.3)
 
 print("Diffusion pipeline loaded")
 
 #AUTO MASK 
-
 def generate_mask(image: Image.Image):
 
     orig_w, orig_h = image.size
@@ -91,20 +101,25 @@ def generate_mask(image: Image.Image):
     return Image.fromarray(mask)
 
 #API FLASK
-
 @app.route("/api/generate", methods=["POST"])
 def api_generate():
+
+    print("FORM DATA:", request.form)
 
     if "person" not in request.files or "cloth" not in request.form:
         return jsonify({"error": "Missing data"}), 400
 
     person_file = request.files["person"]
     cloth_path = request.form["cloth"]
+    username = request.form.get("username")
 
-    person = Image.open(person_file).convert("RGB").resize((512, 512))
-    cloth = Image.open(cloth_path).convert("RGB").resize((512, 512))
+    progress_dict[username] = 0
 
-    mask = generate_mask(person).resize((512, 512))
+    person = Image.open(person_file).convert("RGB").resize((600, 600), Image.LANCZOS)
+    cloth = Image.open(cloth_path).convert("RGB").resize((600, 600), Image.LANCZOS)
+
+    mask = generate_mask(person)
+    mask = mask.resize((600, 600), Image.NEAREST)
 
     person_np = np.array(person)
     mask_np = np.array(mask)
@@ -119,6 +134,11 @@ def api_generate():
     seed = torch.randint(0, 1_000_000, (1,)).item()
     generator = torch.Generator(device).manual_seed(seed)
 
+    def progress_callback(step, timestep, latents):
+        percent = int((step / 50) * 100)
+        progress_dict[username] = percent
+
+
     result = pipe(
         prompt="A photo of the same person wearing the exact clothing from the reference image, natural lighting, detailed fabric, high quality",
         negative_prompt="blurry, distorted body, extra arms, bad anatomy, low quality, redesign, change face",
@@ -127,12 +147,14 @@ def api_generate():
         control_image=canny_image,
         ip_adapter_image=cloth,
         num_inference_steps=50,
-        strength=0.98,
+        strength=0.8,
         generator=generator,
-        guidance_scale=9
+        guidance_scale=9,
+        callback=progress_callback,
+        callback_steps=1
     )
 
-    output_dir = os.path.join("static", "outputs")
+    output_dir = os.path.join("static", "outputs", f"user_{username}")
     os.makedirs(output_dir, exist_ok=True)
 
     filename = f"output_{seed}.png"
@@ -140,11 +162,45 @@ def api_generate():
 
     result.images[0].save(output_path)
 
+    progress_dict[username] = 100
+
+    print("INSERT USER:", username)
+    print("INSERT CLOTH:", cloth_path)
+
+    sql = """
+    INSERT INTO tryon (username, cloth_path, result_img)
+    VALUES (%s, %s, %s)
+    """
+
+    print("USERNAME BEFORE INSERT:", username)
+
+    cursor.execute(sql, (
+    username,
+    cloth_path,
+    filename
+    ))
+
+    db.commit()
+
     return jsonify({
     "status": "success",
-    "image": f"outputs/{filename}",
-    "seed": seed
+    "image": f"outputs/user_{username}/{filename}",
+    "seed": seed,
+    "redirect": "/Trinity%20Fashion%20Group/Pages/user.php"
 })
 
+
+#API HOOK FRONTEND
+@app.route("/api/progress/<username>")
+def get_progress(username):
+
+    progress = progress_dict.get(username, 0)
+
+    print("PROGRESS:", username, progress)
+
+    return jsonify({
+        "progress": progress
+    })
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, threaded=True)
