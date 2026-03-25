b14619b42d52f5d50c9e959cba81118dc73d5b4e
tan <miforbc@gmail.com>
2026-03-25 21:25:03 +0700

    Add responsive and Mobile version for each page, refactor page codes

--- Database/insert_database_admin.php
+++ Database/insert_database_admin.php
@@ -16,20 +16,21 @@ $file = fopen("products.csv", "r");
 while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
 
     $name = $data[0];
-    $price = $data[1];
-    $category = $data[2];
-    $type = $data[3];
-    $description = $data[4];
-    $color = $data[5];
-    $size = $data[6];
-    $front = $data[7];
-    $side = $data[8];
-    $back = $data[9];
+    $group = $data[1];
+    $price = $data[2];
+    $category = $data[3];
+    $type = $data[4];
+    $description = $data[5];
+    $color = $data[6];
+    $size = $data[7];
+    $front = $data[8];
+    $side = $data[9];
+    $back = $data[10];
 
     $sql = "INSERT INTO products 
-            (product_name, product_price, product_category, product_type, product_describe, product_color, product_size, product_img, product_img1, product_img2)
+            (product_name, product_group, product_price, product_category, product_type, product_describe, product_color, product_size, product_img, product_img1, product_img2)
             VALUES 
-            ('$name','$price','$category','$type', '$description','$color','$size','$front','$side','$back')";
+            ('$name','$group','$price','$category','$type', '$description','$color','$size','$front','$side','$back')";
 
     $conn->query($sql);
 }
